842df1f6e2e9d83c7cc9132ba3354007bb7245e4
DanHa Le <dle3@occs.cs.oberlin.edu>
2024-04-30 11:13:16 -0400

    added comments

--- dataparser.py
+++ dataparser.py
@@ -6,11 +6,9 @@ def main():
     #read in the file
     path = sys.argv[1] #file name
     csv = pandas.read_csv(path)
-
+	#open and write file
     f = open("finetuner.jsonl", "w")
-    writer = ""
-
-    print(len(csv))
+    #inputting the text into the file
     for i in range (len(csv)):
         writer = "{{\"input\": \"{}\", \"output\": \"{}\"}}\n".format(csv["product"][i][1:], csv["ad"][i])
         f.write(writer)
