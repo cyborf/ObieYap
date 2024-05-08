c9933f8f00a2b485829572134a98ab465b2649c4
DanHa Le <dle3@ssdl137-24.public.oberlin.edu>
2024-05-08 18:08:35 -0400

    parser for hugging face csv

--- dataparser.py
+++ dataparser.py
@@ -5,14 +5,26 @@ def main():
 
     #read in the file
     path = sys.argv[1] #file name
+    perc = float(sys.argv[2]) #training percentage
     csv = pandas.read_csv(path)
+
+    point = int(perc * len(csv))
+
 	#open and write file
-    f = open("finetuner.jsonl", "w")
+    f = open("{}_training.jsonl".format(path.split(".")[0]), "w")
     #inputting the text into the file
-    for i in range (len(csv)):
-        writer = "{{\"input\": \"{}\", \"output\": \"{}\"}}\n".format(csv["product"][i][1:], csv["ad"][i])
+    for i in range (0, point):
+        writer = "{{\"input\": \"{}\", \"output\": \"{}\"}}\n".format(csv["product"][i][1:].replace('\"', ''), csv["ad"][i].replace('\"', ''))
         f.write(writer)
-        
+    f.close()
+
+    f = open("{}_validation.jsonl".format(path.split(".")[0]), "w")
+    #inputting the text into the file
+    for i in range (point, len(csv)):
+        writer = "{{\"input\": \"{}\", \"output\": \"{}\"}}\n".format(csv["product"][i][1:].replace('\"', ''), csv["ad"][i].replace('\"', ''))
+        f.write(writer)
+    f.close()
     
+
 
 main()
